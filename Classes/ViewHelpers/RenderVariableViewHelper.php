9a844c599f2a8b3033db5bd834b62dbb8d6e0bd9
Tim Rücker <tim.ruecker@wendweb.de>
2018-12-12 19:43:20 +0100

    Adding Viewhelper to allow rendering of variables inside of partials that should definitely not be replaced by javascript template variables

---
+++ Classes/ViewHelpers/RenderVariableViewHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace iizunats\iizuna\ViewHelpers;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+
+
+
+class RenderVariableViewHelper extends AbstractViewHelper {
+
+	/**
+	 * @return void
+	 */
+	public function initializeArguments () {
+		$this->registerArgument('name', 'string', 'Name of variable to render', true);
+	}
+
+
+	/**
+	 * Default render method to render ViewHelper with
+	 * first defined optional argument as content.
+	 *
+	 * @return string Rendered string
+	 * @api
+	 */
+	public function render () {
+		return static::renderStatic(
+			$this->arguments,
+			$this->buildRenderChildrenClosure(),
+			$this->renderingContext
+		);
+	}
+
+
+	/**
+	 * @param array $arguments
+	 * @param \Closure $renderChildrenClosure
+	 * @param RenderingContextInterface $renderingContext
+	 *
+	 * @return mixed
+	 */
+	public static function renderStatic (array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext) {
+		return $renderingContext->getVariableProvider()->get($arguments['name']);
+	}
+}
