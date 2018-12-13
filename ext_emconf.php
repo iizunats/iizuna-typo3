b471ccc0616ff877d7ef8ad5d355556960373648
nano1237 <tim.ruecker@ymail.com>
2018-12-14 00:01:17 +0100

    releasing beta

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF['iizuna'] = [
 	'title'            => 'iizuna Template API',
 	'description'      => 'A TYPO3 Extension that allows the use of server-side rendered but client side hydrated applications.',
 	'category'         => 'fe',
-	'version'          => '0.0.1',
-	'state'            => 'alpha',
+	'version'          => '0.1.0',
+	'state'            => 'beta',
 	'clearcacheonload' => 1,
 	'author'           => 'Tim Rücker',
 	'author_email'     => 'tim.ruecker@iizunats.com',
 	'constraints'      => [
 		'depends' => [
-			'typo3' => '7.6.0-9.5.99',
+			'typo3' => '6.2.0-9.5.99',
 			'php'   => '7.0.0-0.0.0',
 		],
 	],
